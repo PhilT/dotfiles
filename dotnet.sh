05a3ddae79c0ab059f9e1ac0fe381ecdccfd1600
Phil Thompson <phil@electricvisions.com>
2021-02-01 09:43:27 +0000

    Script to install .NET SDK and Opt out of MS telemetry

---
+++ dotnet.sh
@@ -0,0 +1,7 @@
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+
+sudo apt update
+sudo apt install -y apt-transport-https
+sudo apt update
+sudo apt install -y dotnet-sdk-3.1
--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ sudo apt install npm
 sudo npm install -g --save typescript-language-server
 
 ./create_links.sh
+./dotnet.sh
 
