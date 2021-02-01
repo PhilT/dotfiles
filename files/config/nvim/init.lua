cfb31f7f5f6d7b3d697e8c1b84e93595cdab92c0
Phil Thompson <phil@electricvisions.com>
2021-02-01 21:24:45 +0000

    Formatting

--- files/config/nvim/init.lua
+++ files/config/nvim/init.lua
@@ -123,7 +123,7 @@ g.lightline = {
   component_function = { filename = 'FilenameForLightline' }                    -- Calls filename_for_lightline function to show full path name in statusline 
 }
 
-shellcmd = windows and 'term://pwsh -C' or 'term://'                               -- Used by terminal split mappings to use shellcmd as the terminal shell
+shellcmd = windows and 'term://pwsh -C' or 'term://'                            -- Used by terminal split mappings to use shellcmd as the terminal shell
 g.diagnostic_insert_delay = 1
 g.completion_enable_auto_popup = 0                                              -- LSP completion: Disable auto popup of completion
 g.completion_enable_snippet = 'UltiSnips'                                       -- LSP completion: Enable UltiSnips integration
@@ -193,7 +193,7 @@ function run_completion_or_next_or_snippet()
   endif                                                                         -- behind the current position
 end
 
-function previous_completion_item()                                               -- If completion list is open go to the previous item in the list
+function previous_completion_item()                                             -- If completion list is open go to the previous item in the list
   return pumvisible() ? '\<C-p>' : '\<S-Tab>'                                   -- otherwise send SHIFT+TAB
 end
 --]]
@@ -214,67 +214,67 @@ map('n', 'k', 'gk')                                                             
 map('n', 'go', '<cmd>Scratch<CR>')                                              -- go to switch to Scratch window (closes when switching to another window)
 map('n', 'gp', '<cmd>ScratchPreview<CR>')                                       -- gp to open Scratch window
 
---nnoremap <C-z> :CocCommand snippets.editSnippets<CR>|                           -- CTRL+z to edit snippets for current file type
+--nnoremap <C-z> :CocCommand snippets.editSnippets<CR>|                         -- CTRL+z to edit snippets for current file type
 
 
-map('n', 'zz', '<c-w>_ \\| <c-w>\\|')                                                   -- Zoom in and maximize current window
-map('n', 'zo', '<c-w>=')                                                              -- Zoom out and equalize windows
+map('n', 'zz', '<c-w>_ \\| <c-w>\\|')                                           -- Zoom in and maximize current window
+map('n', 'zo', '<c-w>=')                                                        -- Zoom out and equalize windows
 
-map('n', 'ta', '<cmd>tabe<CR>')                                                          -- Add tab pane
-map('n', 'tc', '<cmd>tabc<CR>')                                                          -- Clear (remove) tab pane
-map('n', 'tn', '<cmd>tabn<CR>')                                                          -- Next tab pane
-map('n', 'tp', '<cmd>tabp<CR>')                                                          -- Previous tab pane
-map('n', 'tt', '<cmd>tabe<CR><cmd>edit '..shellcmd..'<CR>')                              -- Open terminal in new tab
+map('n', 'ta', '<cmd>tabe<CR>')                                                 -- Add tab pane
+map('n', 'tc', '<cmd>tabc<CR>')                                                 -- Clear (remove) tab pane
+map('n', 'tn', '<cmd>tabn<CR>')                                                 -- Next tab pane
+map('n', 'tp', '<cmd>tabp<CR>')                                                 -- Previous tab pane
+map('n', 'tt', '<cmd>tabe<CR><cmd>edit '..shellcmd..'<CR>')                     -- Open terminal in new tab
 
-map('n', '<CR>', '<cmd>cn<CR>')                                                          -- Next quickfix entry
-map('n', '<Leader><CR>', '<cmd>cp<CR>')                                                  -- Previous quickfix entry
+map('n', '<CR>', '<cmd>cn<CR>')                                                 -- Next quickfix entry
+map('n', '<Leader><CR>', '<cmd>cp<CR>')                                         -- Previous quickfix entry
 
-map('n', '<F6>', '<cmd>setlocal spell!<CR>')                                             -- Toggle spellcheck
-map('n', '<F7>', '<cmd>call v:lua.delete_term_buffer()<CR>')                            -- Close build/test terminal split
-map('n', '<F8>', "<cmd>call v:lua.term_run('build')<CR>")                                  -- Runs ./build in a terminal split
-map('n', '<F9>', "<cmd>call v:lua.term_run('test')<CR>")                                   -- Run tests in a terminal
-map('n', '<S-F9>', "<cmd>call v:lua.term_run('test', 'GRAPH')<CR>")                        -- Run tests in a terminal with GRAPH directive
-map('n', '<F10>', "<cmd>call v:lua.term_run('run')<CR>")                                   -- Run the project
-map('n', '<S-F10>', "<cmd>call v:lua.term_run('run_release')<CR>")                         -- Run the project
-map('n', '<Leader><F10>', "<cmd>call v:lua.term_run('bench')<CR>")                         -- Run BenchmarkDotNet
-map('n', '<F11>', "<cmd>call v:lua.term_run('clean')<CR>")                                 -- Clean the project
+map('n', '<F6>', '<cmd>setlocal spell!<CR>')                                    -- Toggle spellcheck
+map('n', '<F7>', '<cmd>call v:lua.delete_term_buffer()<CR>')                    -- Close build/test terminal split
+map('n', '<F8>', "<cmd>call v:lua.term_run('build')<CR>")                       -- Runs ./build in a terminal split
+map('n', '<F9>', "<cmd>call v:lua.term_run('test')<CR>")                        -- Run tests in a terminal
+map('n', '<S-F9>', "<cmd>call v:lua.term_run('test', 'GRAPH')<CR>")             -- Run tests in a terminal with GRAPH directive
+map('n', '<F10>', "<cmd>call v:lua.term_run('run')<CR>")                        -- Run the project
+map('n', '<S-F10>', "<cmd>call v:lua.term_run('run_release')<CR>")              -- Run the project
+map('n', '<Leader><F10>', "<cmd>call v:lua.term_run('bench')<CR>")              -- Run BenchmarkDotNet
+map('n', '<F11>', "<cmd>call v:lua.term_run('clean')<CR>")                      -- Clean the project
 
-map('n', '[g', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')               -- diagnostic-nvim: Navigate diagnostics
-map('n', ']g', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')               -- diagnostic-nvim: Navigate diagnostics
-map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')                     -- LSP: Goto definition
-map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')                     -- LSP: List references - ENTER to go to next
-map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')                            -- LSP: Show documentation
-map('n', 'X', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>')     -- LSP: Show errors in floating window (in case of long lines)
-map('i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')              -- LSP: Show signature (only works when opening parens)
+map('n', '[g', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')                   -- diagnostic-nvim: Navigate diagnostics
+map('n', ']g', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')                   -- diagnostic-nvim: Navigate diagnostics
+map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')                         -- LSP: Goto definition
+map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')                         -- LSP: List references - ENTER to go to next
+map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')                               -- LSP: Show documentation
+map('n', 'X', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>')        -- LSP: Show errors in floating window (in case of long lines)
+map('i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')                  -- LSP: Show signature (only works when opening parens)
 
-map('n', 'gD', '<cmd>lua vim.lsp.buf.implementation()<CR>')              -- LSP: Not supported by F# LS
+map('n', 'gD', '<cmd>lua vim.lsp.buf.implementation()<CR>')                     -- LSP: Not supported by F# LS
 
-map('t', '<A-e>', '<C-\\><C-n>')                                                     -- ALT+e switches to NORMAL mode from TERMINAL mode
-map('t', '<A-h>', '<C-\\><C-n><C-w>h')                                               --
-map('t', '<A-j>', '<C-\\><C-n><C-w>j')                                               -- ALT+(hjkl) to navigate splits from any mode
-map('t', '<A-k>', '<C-\\><C-n><C-w>k')                                               -- including TERMINAL mode.
-map('t', '<A-l>', '<C-\\><C-n><C-w>l')                                               --
-map('i', '<A-h>', '<C-\\><C-n><C-w>h')                                               -- CTRL+(hjkl) to navigate splits from NORMAL mode
-map('i', '<A-j>', '<C-\\><C-n><C-w>j')                                               --
-map('i', '<A-k>', '<C-\\><C-n><C-w>k')                                               --
-map('i', '<A-l>', '<C-\\><C-n><C-w>l')                                               --
-map('n', '<A-h>', '<C-w>h')                                                         --
-map('n', '<A-j>', '<C-w>j')                                                         --
-map('n', '<A-k>', '<C-w>k')                                                         --
-map('n', '<A-l>', '<C-w>l')                                                         --
-map('n', '<C-h>', '<C-w>h')                                                         --
-map('n', '<C-j>', '<C-w>j')                                                         --
-map('n', '<C-k>', '<C-w>k')                                                         --
-map('n', '<C-l>', '<C-w>l')                                                         --
+map('t', '<A-e>', '<C-\\><C-n>')                                                -- ALT+e switches to NORMAL mode from TERMINAL mode
+map('t', '<A-h>', '<C-\\><C-n><C-w>h')                                          --
+map('t', '<A-j>', '<C-\\><C-n><C-w>j')                                          -- ALT+(hjkl) to navigate splits from any mode
+map('t', '<A-k>', '<C-\\><C-n><C-w>k')                                          -- including TERMINAL mode.
+map('t', '<A-l>', '<C-\\><C-n><C-w>l')                                          --
+map('i', '<A-h>', '<C-\\><C-n><C-w>h')                                          -- CTRL+(hjkl) to navigate splits from NORMAL mode
+map('i', '<A-j>', '<C-\\><C-n><C-w>j')                                          --
+map('i', '<A-k>', '<C-\\><C-n><C-w>k')                                          --
+map('i', '<A-l>', '<C-\\><C-n><C-w>l')                                          --
+map('n', '<A-h>', '<C-w>h')                                                     --
+map('n', '<A-j>', '<C-w>j')                                                     --
+map('n', '<A-k>', '<C-w>k')                                                     --
+map('n', '<A-l>', '<C-w>l')                                                     --
+map('n', '<C-h>', '<C-w>h')                                                     --
+map('n', '<C-j>', '<C-w>j')                                                     --
+map('n', '<C-k>', '<C-w>k')                                                     --
+map('n', '<C-l>', '<C-w>l')                                                     --
 
 map('n', '<Leader>a', '<cmd>source $HOME/.config/nvim/init.vim<CR>')                     -- Reload Vim config
 map('n', '<Leader>d', '<cmd>bd<CR>')                                            -- delete buffer
 map('n', '<Leader>dd', '<cmd>bufdo bd<CR>')                                     -- delete all buffers
-map('n', '<Leader>h', '<cmd>topleft vsplit '..shellcmd..'<CR>')           -- Terminal far left split
+map('n', '<Leader>h', '<cmd>topleft vsplit '..shellcmd..'<CR>')                 -- Terminal far left split
 map('n', '<Leader>i', '<cmd>setlocal number!<CR>')                              -- Toggle line numbers
-map('n', '<Leader>j', '<cmd>botright split '..shellcmd..'<CR>')           -- Terminal bottom split
-map('n', '<Leader>k', '<cmd>topleft split '..shellcmd..'<CR>')            -- Terminal top split
-map('n', '<Leader>l', '<cmd>botright vsplit '..shellcmd..'<CR>')          -- Terminal far right split
+map('n', '<Leader>j', '<cmd>botright split '..shellcmd..'<CR>')                 -- Terminal bottom split
+map('n', '<Leader>k', '<cmd>topleft split '..shellcmd..'<CR>')                  -- Terminal top split
+map('n', '<Leader>l', '<cmd>botright vsplit '..shellcmd..'<CR>')                -- Terminal far right split
 map('n', '<Leader>n', '<cmd>bn<CR>')                                            -- next buffer
 map('n', '<Leader>o', '<cmd>set paste!<CR>')                                    -- Toggle paste formatting
 map('n', '<Leader>p', '<cmd>bp<CR>')                                            -- previous buffer
@@ -301,10 +301,10 @@ local function au(group, commands)
 end
 
 local commands = {
-  'BufReadPost quickfix nnoremap <buffer> <CR> <CR>',               -- Unmap ENTER in Quickfix so found files can be selected for example
-  [[FileType fsharp let b:AutoPairs = AutoPairsDefine({'(*' : '*)//n'})]], -- Define auto-pair for F# multiline comments
-  'BufLeave todo.txt resize 2 | normal gg',                         -- Resize todo window on leaving
-  'BufEnter todo.txt resize 20 | set wfh',                          -- Resize todo window on entering
+  'BufReadPost quickfix nnoremap <buffer> <CR> <CR>',                           -- Unmap ENTER in Quickfix so found files can be selected for example
+  [[FileType fsharp let b:AutoPairs = AutoPairsDefine({'(*' : '*)//n'})]],      -- Define auto-pair for F# multiline comments
+  'BufLeave todo.txt resize 2 | normal gg',                                     -- Resize todo window on leaving
+  'BufEnter todo.txt resize 20 | set wfh',                                      -- Resize todo window on entering
   'Filetype fsharp setlocal omnifunc=v:lua.vim.lsp.omnifunc',
   'Filetype typescript setlocal omnifunc=v:lua.vim.lsp.omnifunc',
   [[bufread *.fsx,*.fs syn region fsharpMultiLineComment start='(\*' end='\*)' contains=fsharpTodo,@Spell]] -- Spell checking in multi-line F# comments
@@ -319,21 +319,21 @@ function set_term_insert()                                                      
 end
 set_term_insert()                                                               -- Make insert mode for starting terminals the default
 
-function _G.switch_out_of_term()                                                      -- Switch back to previous window after running build command
-  g.termbuffer = vim.fn.bufnr('%')                                                 -- Store terminal buffer used to run build
-  cmd('normal G')                                                           -- Move to bottom of output
-  cmd('wincmd p')                                                                      -- Switch to previous window
-  set_term_insert()                                                          -- Reset back to insert mode when starting terminal window
+function _G.switch_out_of_term()                                                -- Switch back to previous window after running build command
+  g.termbuffer = vim.fn.bufnr('%')                                              -- Store terminal buffer used to run build
+  cmd('normal G')                                                               -- Move to bottom of output
+  cmd('wincmd p')                                                               -- Switch to previous window
+  set_term_insert()                                                             -- Reset back to insert mode when starting terminal window
 end
 
-function _G.term_run(command)                                                           -- Run a build command e.g. 'run', 'test', 'build'
+function _G.term_run(command)                                                   -- Run a build command e.g. 'run', 'test', 'build'
   delete_term_buffer()
 
   au('TermInsert', {'TermOpen * call v:lua.switch_out_of_term()'})
   cmd('botright split '..shellcmd..' ./'..command)
 end
 
-function _G.delete_term_buffer()                                                     -- Removes the terminal buffer used to run commands
+function _G.delete_term_buffer()                                                -- Removes the terminal buffer used to run commands
   if termbuffer then
     cmd(termbuffer..'bd!')
     termbuffer = nil
