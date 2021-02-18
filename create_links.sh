10970826b3f8d7618222ab0913d60ed2c6273c85
Phil Thompson <phil@electricvisions.com>
2021-02-18 20:43:36 +0000

    Make sure all links are removed and created

--- create_links.sh
+++ create_links.sh
@@ -1,5 +1,14 @@
-rm -f $HOME/.config/nvim/init.vim && ln -s $CODE_DIR/dotfiles/files/common/init.lua $HOME/.config/nvim/init.lua
-rm -f $HOME/.config/nvim/init.lua && ln -s $CODE_DIR/dotfiles/files/common/init.vim $HOME/.config/nvim/init.vim
-rm -f $HOME/.bashrc && ln -s $CODE_DIR/dotfiles/files/linux/bashrc $HOME/.bashrc
-rm -f $HOME/.tmux.conf && ln -s $CODE_DIR/dotfiles/files/linux/tmux.conf $HOME/.tmux.conf
+#rm -f $HOME/.config/nvim/init.vim && ln -s $CODE_DIR/dotfiles/files/common/init.lua $HOME/.config/nvim/init.lua
+rm -f $HOME/.config/nvim/init.lua
+rm -f $HOME/.gitconfig
+rm -f $HOME/.gitignore
+rm -f $HOME/.ignore
+rm -f $HOME/.bashrc
+rm -f $HOME/.tmux.conf
 
+ln -s $CODE_DIR/dotfiles/files/common/init.lua $HOME/.config/nvim/init.lua
+ln -s $CODE_DIR/dotfiles/files/common/gitconfig $HOME/.gitconfig
+ln -s $CODE_DIR/dotfiles/files/common/gitignore $HOME/.gitignore
+ln -s $CODE_DIR/dotfiles/files/common/ignore $HOME/.ignore
+ln -s $CODE_DIR/dotfiles/files/linux/bashrc $HOME/.bashrc
+ln -s $CODE_DIR/dotfiles/files/linux/tmux.conf $HOME/.tmux.conf
