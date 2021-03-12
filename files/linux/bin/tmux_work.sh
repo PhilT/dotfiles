e10af959799b2d4ce7fa0b77599f9a60ae4a1469
Phil Thompson <phil@electricvisions.com>
2021-03-12 09:19:18 +0000

    git sd - stash staged only

--- files/linux/bin/tmux_work.sh
+++ files/linux/bin/tmux_work.sh
@@ -2,6 +2,6 @@ tmux \
   new-session  -c $WORK_DIR -s work 'bash --init-file <(echo ". \"$HOME/.bashrc\"; nvim; pwd")' \; \
   split-window -c $WORK_DIR -v -l 20 \; \
   split-window -c $WORK_DIR -h -l 70 '/usr/bin/redis-server --databases 26' \; \
-  split-window -c $WORK_DIR -v -p 80 'chruby-exec ruby-2.6.3 -- bin/rails s' \; \
+  split-window -c $WORK_DIR -v -p 80 'chruby-exec ruby-2.6.6 -- bin/rails s' \; \
   select-pane -t 0 \; \
   attach
