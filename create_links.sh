74874b1688bfba0311a98ff31d4abdff8d469508
Phil Thompson <phil@electricvisions.com>
2023-05-17 20:55:11 +0100

    Add alacritty (shell) config, swap CTRL and CAPSLOCK

--- create_links.sh
+++ create_links.sh
@@ -4,6 +4,7 @@ rm -f $HOME/.gitconfig
 rm -f $HOME/.gitignore
 rm -f $HOME/.ignore
 rm -f $HOME/.bashrc
+rm -f $HOME/.alacritty.yml
 rm -f $HOME/.tmux.conf
 rm -f $HOME/bin
 
@@ -15,5 +16,6 @@ ln -s $CODE_DIR/dotfiles/files/common/gitconfig $HOME/.gitconfig
 ln -s $CODE_DIR/dotfiles/files/common/gitignore $HOME/.gitignore
 ln -s $CODE_DIR/dotfiles/files/common/ignore $HOME/.ignore
 ln -s $CODE_DIR/dotfiles/files/linux/bashrc $HOME/.bashrc
+ln -s $CODE_DIR/dotfiles/files/linux/alacritty.yml $HOME/.alacritty.yml
 ln -s $CODE_DIR/dotfiles/files/linux/tmux.conf $HOME/.tmux.conf
 ln -s $CODE_DIR/dotfiles/files/linux/bin $HOME/bin
