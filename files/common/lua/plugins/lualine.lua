2c2b16e6a7481b5bea40668e8e5d03a2910d1cbe
Phil Thompson <phil@electricvisions.com>
2023-07-25 08:24:27 +0100

    More tweaks to lualine

--- files/common/lua/plugins/lualine.lua
+++ files/common/lua/plugins/lualine.lua
@@ -1,6 +1,8 @@
 require('lualine').setup {
   options = {
     icons_enabled = false,
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
   },
   sections = {
     lualine_a = {'mode'},
@@ -9,5 +11,8 @@ require('lualine').setup {
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'},
+  },
+  inactive_sections = {
+    lualine_c = {{'filename', path = 1}},
   }
 }
--- files/common/lua/plugins/purescript.lua
+++ files/common/lua/plugins/purescript.lua
@@ -1,5 +1,6 @@
 require'lspconfig'.purescriptls.setup {
   --on_attach = on_attach,
+  cmd = { "npx", "purescript-language-server", "--stdio" },
   settings = {
     purescript = {
       addSpagoSources = true -- e.g. any purescript language-server config here
